6f104c93e00e374439379e8666e9cf73d4bd1623
rinki <rinkikhandal2000@gmail.com>
2023-10-17 22:13:30 +0530

    changes in webpack

---
+++ server/webpack.dev.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const NodemonPlugin = require("nodemon-webpack-plugin");
+
+module.exports = {
+  target: "node",
+  resolve: {
+    extensions: [".js", ".jsx"],
+    fallback: {
+      "mongodb-client-encryption": false,
+      aws4: false,
+      snappy: false,
+      "@mongodb-js/zstd": false,
+      kerberos: false,
+      "@aws-sdk/credential-providers": false,
+      "mock-aws-s3": false,
+      nock: false,
+      "aws-sdk": false,
+      esbuild: false,
+      "@swc/core": false,
+      "uglify-js": false,
+    },
+  },
+  entry: "./server.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: "source-map",
+  plugins: [new NodemonPlugin()],
+};
